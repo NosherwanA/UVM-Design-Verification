400d868cf93d1e68f6773f9bcffc9b1517442b94
NosherwanA <nosherwan.a2@gmail.com>
2019-11-03 13:02:19 -0500

    Modified sram module signature

--- rtl/sram.v
+++ rtl/sram.v
@@ -4,26 +4,41 @@
 // 2 November 2019
 
 module sram(
-    input wire clk,                         // Input Clock
-    input wire [ADDR_WIDTH-1:0] addr,       // Input Address
-    input wire rwb,                         // Input Read Write Bit
-    inout wire [DATA_WIDTH-1:0] data        // Inout Data
+    clk,                                    // Input Clock
+    addr,                                   // Input Address
+    cs,                                     // Input Chip Select
+    rwb,                                    // Input Read Write Bit
+    data_i,                                 // Input Data
+    data_o                                  // Output Data
 );
     // Parameters
     parameter ADDR_WIDTH = 8;               // Address Bus Width
     parameter DATA_WIDTH = 32;              // Data Bus Width
     parameter DEPTH = 1 << ADDR_WIDTH;      // SRAM Depth
 
+    // Input Ports
+    input wire clk;                         // Clock
+    input wire [ADDR_WIDTH-1:0] addr;       // Address
+    input wire cs;                          // Chip Select
+    input wire rwb;                         // Read Write Bit
+    input wire [DATA_WIDTH-1:0] data_i;     // Input Data
+
+    // Output Ports
+    output reg [DATA_WIDTH-1:0] data_o;    // Output Data       
+
+
     // Memory Array
     reg [DATA_WIDTH-1:0] mem [0:DEPTH-1]; 
 
     always@(posedge clk)
     begin
-        if (rwb) begin
-            mem[addr] <= data;
-        end
-        else
-            data <= mem[addr];
+        if (cs) begin
+            if (rwb) begin
+                mem[addr] <= data_i;
+            end
+            else begin
+                data_o <= mem[addr];
+            end
         end
     end
 endmodule // sram
