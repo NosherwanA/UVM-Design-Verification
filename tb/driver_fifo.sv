4047c1cf0bad679a32f70cf91ba8ef37a0f538a2
NosherwanA <nosherwan.a2@gmail.com>
2019-11-23 10:46:41 -0500

    Finished the UVM dirver for FIFO

--- tb/driver_fifo.sv
+++ tb/driver_fifo.sv
@@ -26,14 +26,60 @@ class fifo_driver extends uvm_driver #(fifo_seq_item);
     endfunction //connect_phase
 
     task run_phase (uvm_phase phase);
-        forever begin
-            seq_item_port.get_next_item(req);
 
-            
+        driver_if.reset_n <= 1;
+        fork
+            forever begin
+                seq_item_port.get_next_item(req);
 
+                case(req.command)
 
-            seq_item_port.item_done(); 
-        end   
+                    Reset: begin
+                        // Pull reset signal down for 3 clk cycles and release the reset
+                        #1
+                        driver_if.re <= 0;
+                        driver_if.we <= 0;
+                        driver_if.reset_n <= req.reset_n;
+                        repeat(3) @(posedge driver_if.clk) #1;
+                        driver_if.reset_n <= 1;
+                        @(posedge driver_if.clk) #1 
+                    end
+                    Write: begin
+                        // Writing Data to Fifo
+                        driver_if.we <= req.we;
+                        driver_if.data_in <= req.data_in;
+                        @(posedge driver_if.clk) #1;
+                        driver_if.we <= 0;
+                    end
+                    Read: begin
+                        // Reading Data from Fifo
+                        driver_if.re <= req.re;
+                        @(posedge driver_if.clk) #1;
+                        driver_if.re <= 0;
+                    end
+                    Full: begin
+                        // Checking if the full signal from fifo is asserted
+                        if (driver_if.full !== req.full) begin
+                            resp = new();               // Returning the actual state of full signal of fifo
+                            resp.full <= driver_if.full;
+                            resp.timestamp = $realtime; 
+                        end
+                    end
+                    Empty: begin
+                        // Checking if the empty signal from fifo is asserted
+                        if (driver_if.empty !== req.empty) begin
+                            resp = new();               // Returning the actual state of empty signal of fifo
+                            resp.empty <= driver_if.empty;
+                            resp.timestamp = $realtime; 
+                        end
+                    end
+
+                endcase
+
+                seq_item_port.item_done(); 
+            end   
+        join
+        
     endtask // run_phase
 
 endclass //fifo_driver extends uvm_driver
